c8e36958c385c00da1706cb23380bf6b8fb13ec5
Dario Podhorski <dpodhorski@gmail.com>
2016-05-13 13:17:38 +0200

    Update to MVC
    Update to  MVC. Index1.php set to be controller. Added view.php and
    logout.php

--- admin_page.php
+++ admin_page.php
@@ -1,3 +1,14 @@
 <?php
 echo "Ovo su administratorske stranice";
- ?>
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+    <a href="logout.php">Logout</a>
+    </body>
+</html>
--- user_page.php
+++ user_page.php
@@ -1,3 +1,15 @@
 <?php
 echo "Ovo su stranice za obicnog usera";
  ?>
+
+
+ <!DOCTYPE html>
+ <html>
+     <head>
+         <meta charset="utf-8">
+         <title></title>
+     </head>
+     <body>
+     <a href="logout.php">Logout</a>
+     </body>
+ </html>
