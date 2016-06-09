50f56d1332d6f4732bcc820ce388a7bcdb5a3d85
Dario Podhorski <dpodhorski@gmail.com>
2016-06-09 12:49:29 +0200

    ispravak
    izbacivanje github komentara

--- login.php
+++ login.php
@@ -22,18 +22,11 @@ if ($link->connect_error) { //check database connection
 }
 
 
-
 /**
 * Get and set variables
-<<<<<<< HEAD
-**********************/
+*/
 $username = $_SESSION['username'];
 $password = $_SESSION['password'];
-=======
-*/
-$username = $_POST['username'];
-$password = $_POST['password'];
->>>>>>> refs/remotes/origin/master
 $res_query = '';
 
 
@@ -46,10 +39,6 @@ JOIN password on korisnici.id_korisnik = password.id_korisnik
 WHERE email =? AND password= ?";
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/master
 /**
 *Perform query on database
 */
