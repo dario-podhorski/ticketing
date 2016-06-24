6e47ccfbe168a8df91c809467477ab2ba405efdd
Dario Podhorski <dpodhorski@gmail.com>
2016-06-24 10:35:22 +0200

    user class
    Added construct to user class

--- user.php
+++ user.php
@@ -10,6 +10,16 @@ class user {
     protected $id;
     protected $name;
     protected $surName;
+    protected $email;
+    protected $phone;
+    protected $city;
     
-    
+    function __construct($id, $name, $surName, $email, $phone, $city) {
+        $this->id=$id;
+        $this->name=$name;
+        $this->surName=$surName;
+        $this->email=$email;
+        $this->phone=$phone;
+        $this->city=$city;
+    }
 }
