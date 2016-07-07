e16db4f0b795d252ed7e74c774542d4fc2cdf13f
Dario Podhorski <dpodhorski@gmail.com>
2016-07-07 15:47:23 +0200

    change to DB connection class

--- database/db_conn.php
+++ database/db_conn.php
@@ -1,6 +1,24 @@
 <?php
-$host = 'localhost';
-$database= 'ticketing';
-$user = 'root';
-$password = 'mysql';
- ?>
+
+namespace database;
+
+class DB {
+    
+    private $host = 'localhost';
+    private $database= 'ticketing';
+    private static $user = 'root';
+    private static $password = 'mysql';
+    private static $_connection;
+            
+    //function __construct() {
+        //$this->_connection = new \PDO("mysql:host=$this->host;dbname=$this->database", $this->user, $this->password);
+        
+    //}
+
+    public static function getConnection(){
+        self::$_connection = new \PDO("mysql:host=localhost;dbname=ticketing", self::$user, self::$password);
+        return self::$_connection;
+    }
+}
+
+ 
