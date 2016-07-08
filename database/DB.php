9e28c0320cd575c53c69ecb52d2197f9e6da29d8
Dario Podhorski <dpodhorski@gmail.com>
2016-07-08 11:25:14 +0200

    change to DB connection class, change to constants

---
+++ database/DB.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace database;
+
+class DB {
+    
+    const host = 'localhost';
+    const database= 'ticketing';
+    const user = 'root';
+    const password = 'mysql';
+    private static $_connection;
+            
+    public static function getConnection(){
+        $_connection = new \PDO('mysql:host='.self::host.';dbname='.self::database, self::user, self::password);
+        return $_connection;
+        
+
+        
+    }
+}
+
+ 
