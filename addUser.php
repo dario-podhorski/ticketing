d6991a006f53f4ef1c33940fac476fae62625a02
Dario Podhorski <dpodhorski@gmail.com>
2016-07-12 14:57:09 +0200

    added addUser form

---
+++ addUser.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3"
+            <form>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Name">
+                    </div>
+                <div class="form-group">
+                    <label for="lastName">Lastname</label>
+                    <input type="text" class="form-control" id="lastName" placeholder="Lastname">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" id="email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="phone">Phone number</label>
+                    <input type="tel" class="form-control" id="phone" placeholder="Phone">
+                </div>
+                <div class="form-group">
+                    <label for="city">City</label>
+                    <select class="form-control" id="city">
+                        <option>Osijek</option>
+                        <option>Zagreb</option>
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
