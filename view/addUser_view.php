01e3596824a880fe87a35886eab723262ec64349
Dario Podhorski <dpodhorski@gmail.com>
2016-07-13 15:18:26 +0200

    create addUser_view(form)

---
+++ view/addUser_view.php
@@ -0,0 +1,32 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3">
+             <form method="post" action="">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" name="name" placeholder="Name">
+                    </div>
+                <div class="form-group">
+                    <label for="lastName">Lastname</label>
+                    <input type="text" class="form-control" name="lastName" placeholder="Lastname">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" class="form-control" name="email" placeholder="Email">
+                </div>
+                <div class="form-group">
+                    <label for="phone">Phone number</label>
+                    <input type="tel" class="form-control" name="phone" placeholder="Phone">
+                </div>
+                <div class="form-group">
+                    <label for="city">City</label>
+                    <select class="form-control" name="city">
+                        <option>Osijek</option>
+                        <option>Zagreb</option>
+                    </select>
+                </div>
+                <button class="btn btn-default" type="submit" value="1" name="sub_bttn">Submit</button>
+            </form>
+        </div>
+    </div>
+</div>
