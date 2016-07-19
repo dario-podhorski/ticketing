8fba18c76afe76d957145ebfe1d691431f1496d1
Dario Podhorski <dpodhorski@gmail.com>
2016-07-19 19:37:13 +0200

    design change

--- view/addUser_view.php
+++ view/addUser_view.php
@@ -3,23 +3,27 @@
         <div class="col-lg-8 col-lg-offset-2" style="background-color: #d9d9d9; border-radius: 15px">
             
              <form method="post" action="">
-                 <div class="col-lg-5">
-                     <div class="row">
+                <div class="row">
+                    <div class="col-lg-5">
+                     
                         <h2><span class="label label-danger">Login info</span></h2>
+                     
                         <div class="form-group">
-                            <label for="email">E-mail</label>
-                            <input type="email" class="form-control" name="email" placeholder="E-mail">
+                            <label for="email">E-mail (required)</label>
+                            <input type="email" class="form-control" name="email" placeholder="E-mail" required>
                         </div>
                         <div class="form-group">
-                            <label for="password">Password</label>
-                            <input type="password" class="form-control" name="password" placeholder="Password">
+                            <label for="password">Password (required)</label>
+                            <input type="password" class="form-control" name="password" placeholder="Password" required>
                         </div>
                     </div>
-                 </div>
+                 
+                                  
                  
                  <div class="col-lg-5 col-lg-offset-1">
-                    <div class="row">
+                    
                         <h2><span class="label label-danger">User info</span></h2>
+                        
                         <div class="form-group">
                             <label for="name">Name</label>
                             <input type="text" class="form-control" name="name" placeholder="Name">
@@ -39,10 +43,13 @@
                                 <option>Zagreb</option>
                             </select>
                         </div>
-                    </div>
-                 </div>
+                </div>
+                 
                 <button class="btn btn-default" type="submit" value="1" name="sub_bttn">Add User</button>
+                </div>
             </form>
+        
+    
         </div>
     </div>
 </div>
