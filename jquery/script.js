485ef4abd9a31608f3096f2087ef8ac23feb2e6d
Dario Podhorski <dpodhorski@gmail.com>
2016-07-20 14:34:23 +0200

    added script.js

---
+++ jquery/script.js
@@ -0,0 +1,21 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function addRecord(){
+    var email = $("#email").val();
+    var password = $("#password").val();
+    var name = $("#name").val();
+    var lastNameame = $("#lastname").val();
+    var phone = $("#phone").val();
+    var city = $("#city").val();
+    
+    if (email == ""){
+        alert("Please enter email");
+    }
+    else if (password == ""){
+        alert("Please enter password")
+    }
+}
+
