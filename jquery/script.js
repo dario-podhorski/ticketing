3965ebdd16a8643123cbc34694e294a0cd3a99a0
Dario Podhorski <dpodhorski@gmail.com>
2016-07-22 13:44:16 +0200

    add ajax

--- jquery/script.js
+++ jquery/script.js
@@ -7,7 +7,7 @@ function addRecord(){
     var email = $("#email").val();
     var password = $("#password").val();
     var name = $("#name").val();
-    var lastNameame = $("#lastname").val();
+    var lastName = $("#lastname").val();
     var phone = $("#phone").val();
     var city = $("#city").val();
     
@@ -15,7 +15,17 @@ function addRecord(){
         alert("Please enter email");
     }
     else if (password == ""){
-        alert("Please enter password")
+        alert("Please enter password");
+    }else {
+        $.post("ajax/create.php", {
+            name: name,
+            lastName: lastName,
+            email: email,
+            phone: phone,
+            city: city
+        },function (data){
+            alert(data);
+        });
     }
 }
 
