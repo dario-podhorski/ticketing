d593720c5840596dfaa75a2b447131176ad5d03f
Dario Podhorski <dpodhorski@gmail.com>
2016-07-28 13:59:24 +0200

    GOT AJAX WORKING

--- jquery/script.js
+++ jquery/script.js
@@ -1,33 +1,41 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Add User jquery script
  */
-function addRecord(){
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var name = $("#name").val();
-    var lastName = $("#lastname").val();
-    var phone = $("#phone").val();
-    var city = $("#city").val();
-    
-    if (email == ""){
-        alert("Please enter email");
-    }
-    else if (password == ""){
-        alert("Please reenter password");
-    }else {
-        $.post("ajax/create.php", {
+
+$(document).ready(function(){
+    $("#add_user").click(function(e){
+        e.preventDefault();
+        var email = $("#email").val();
+        var password = $("#password").val();
+        var name = $("#name").val();
+        var lastName = $("#lastname").val();
+        var phone = $("#phone").val();
+        var city = $("#city").val();
+        
+        if (email == ""){
+            alert("Please enter email");
+        }
+        else if (password == ""){
+            alert("Please reenter password");
+        }else {
+            $.post("ajax/create.php", {
             name: name,
             lastName: lastName,
             email: email,
             phone: phone,
             city: city
             },
-            function(data){
+            function( data ){
             alert(data);
             });
         };
-    }
+    });
+    });
+        
+ 
+
+    
+    
+    
 
 
