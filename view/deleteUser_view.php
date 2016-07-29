413eb6fcda5018df1edeab1bfb6d06bdacb73b70
Dario Podhorski <dpodhorski@gmail.com>
2016-07-29 12:26:30 +0200

    create deleteUser_view

---
+++ view/deleteUser_view.php
@@ -0,0 +1,18 @@
+<div class="container">
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-2">
+            <table class="table">
+                <tr>
+                    <th>User ID</th>
+                    <th>Name</th>
+                    <th>Lastname</th>
+                    <th>E-mail</th>
+                    <th>Phone</th>
+                    <th>City</th>
+                </tr>
+            </table>
+        </div>
+    </div>
+    
+</div>
+ 
