8195d25fa3f5f0905ddbe252927507368e5a4cf9
Dario Podhorski <dpodhorski@gmail.com>
2016-07-29 14:09:10 +0200

    cosmetic

--- view/deleteUser_view.php
+++ view/deleteUser_view.php
@@ -1,15 +1,17 @@
 <div class="container">
     <div class="row">
-        <div class="col-lg-6 col-lg-offset-2">
-            <table class="table">
-                <tr>
-                    <th>User ID</th>
-                    <th>Name</th>
-                    <th>Lastname</th>
-                    <th>E-mail</th>
-                    <th>Phone</th>
-                    <th>City</th>
-                </tr>
+        <div class="col-lg-6 col-lg-offset-3">
+            <table id="usersTable" class="table">
+                <thead>
+                    <tr>
+                        <th>User ID</th>
+                        <th>Name</th>
+                        <th>Lastname</th>
+                        <th>E-mail</th>
+                        <th>Phone</th>
+                        <th>City</th>
+                    </tr>
+                </thead>
             </table>
         </div>
     </div>
